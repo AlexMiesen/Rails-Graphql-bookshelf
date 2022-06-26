f7f21ee9587351db0cac88f86a056ca0dbe6e23a
AlexMiesen <alex@i-motor.com.au>
2022-06-26 23:59:28 +1000

    added the author_type and author field and method to query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -10,11 +10,21 @@ module Types
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator 123" do
-        argument :name, String, required: true
-      end 
+      argument :name, String, required: true
+    end 
+
     def test_field(name:)
       Rails.logger.info context[:time]
       "Hello #{name}!"
     end
+
+    field :author, Types::AuthorType, null: true, 
+      description: "Returns one Author instance"  do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.where(id: id).first
+    end  
   end
 end
