77b7ce8514ddd20177433a90963a77b23c27c839
AlexMiesen <alex@i-motor.com.au>
2022-07-01 09:53:38 +1000

    WIP mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,10 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+
+    field :create_author, Types::AuthorType, null:true do
+      argument :Id, ID,
+
     end
+
   end
 end
