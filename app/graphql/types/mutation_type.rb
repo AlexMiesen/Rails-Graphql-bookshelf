ef9a51b612c270d550b7979b9f0ee69a995ab0ac
AlexMiesen <alex@i-motor.com.au>
2022-07-01 20:05:36 +1000

    create_author mutation added

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,15 @@
 module Types
   class MutationType < Types::BaseObject
 
-    field :create_author, Types::AuthorType, null:true do
-      argument :Id, ID,
-
+    field :create_author, Types::AuthorType, null: true, description:  "a specification" do
+      argument :first_name, String, required: false
+      argument :last_name, String, required: false
+      argument :yob, Int, required: false
+      argument :is_alive, Boolean, required: false 
     end
 
+    def create_author(first_name:, last_name:, yob:, is_alive:)
+      Author.create(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
+    end 
   end
 end
