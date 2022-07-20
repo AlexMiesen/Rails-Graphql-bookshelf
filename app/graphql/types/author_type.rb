58cddf13ed25cd6a2cd0e635f51f0407f08f4873
AlexMiesen <alex@i-motor.com.au>
2022-07-20 17:08:21 +1000

    Removed square brackets in ErrorType. also removed 'attribute' as this is no longer needed in order to show the error

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -5,23 +5,24 @@ class Types::AuthorType < Types::BaseObject
 	field :id, ID, null: false
 	field :first_name, String, null: true
 	field :last_name, String, null: true
-	field :yob, Int, null: true
+	field :yob, Int, null: false
 	field :is_alive, Boolean, null: true
 	field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+
 	field :full_name, String, null: true
 
 	def full_name
 		"#{object.first_name} #{object.last_name}"
 	end
 
-	field :coordinates, Types::CoordinatesType, null: true 
+	field :coordinates, Types::CoordinatesType, null: false 
 
 	field :publication_years, [Int], null: false
 
 	field :errors, [Types::ErrorType], null: true
 
 	def errors
-		object.errors.map { |e| { field_name: e.attribute, errors: object.errors[e.attribute]} }
+		object.errors.map { |e| { field_name: e, errors: object.errors[e]} }
 	end
 
 end 
