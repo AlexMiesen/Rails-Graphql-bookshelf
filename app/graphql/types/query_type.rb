69bac8d788ca48dc19fbd46b30616a4cc8bbbc0b
AlexMiesen <alex@i-motor.com.au>
2022-07-23 12:24:52 +1000

    added a user and session model, users can login with graphql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,16 +7,17 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator 123" do
-      argument :name, String, required: true
-    end 
-
-    def test_field(name:)
-      Rails.logger.info context[:time]
-      "Hello #{name}!"
+    field :login, String, null: true, description: "Login a user" do
+      argument :email, String, required: true
+      argument :password, String, required: true
     end
+
+    def login(email:, password:)
+      if user = User.where(email: email).first&.authenticate(password)
+        user.sessions.create.key
+      end 
+    end
+
 
     field :author, Types::AuthorType, null: true, 
       description: "Returns one Author instance"  do
